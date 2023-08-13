afe0eb8c2a6a360f378c5ba6507ba201d9f18fad
histortgan <rizqiirkhammaulana@gmail.com>
2023-08-13 20:48:05 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY >0)
+});
+
+
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc(){
+    let parent = this.parentNode;
+    parent.classList.add("focus");
+}
+
+function blurFunc(){
+    let parent = this.parentNode;
+    if(this.value == ""){
+        parent.classList.remove("focus");
+
+    }
+} 
+
+inputs.forEach(input => {
+    input.addEventListener("focus", focusFunc);
+    input.addEventListener("blur", blurFunc);
+});
+
+const observer = new IntersectionObserverEntry((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry,isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden')
